ab67d38f0de14b2b01185520e5fdaf6c59d75262
lgy <19977439879@163.com>
2021-03-06 23:45:55 +0800

    first commit

---
+++ Assets/Demigiant/Editor/WelcomeScreen.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class Appload
+{
+    static Appload()
+    {
+        bool hasKey = PlayerPrefs.HasKey("sikiwelcomescreen");
+        if (hasKey==false)
+        {
+            //EditorApplication.update += Update;
+            //PlayerPrefs.SetInt("sikiwelcomescreen", 1);
+            //WelcomeScreen.ShowWindow();
+        }
+    }
+}
+
+public class WelcomeScreen : EditorWindow
+{
+#if false
+    private Texture mSamplesImage;
+    private Rect imageRect = new Rect(30f, 90f, 350f, 350f);
+    private Rect textRect = new Rect(15f, 15f, 380f, 100f);
+
+    public void OnEnable()
+    {
+        this.mSamplesImage = LoadTexture("wechat.jpg");
+    }
+
+    
+    Texture LoadTexture(string name)
+    {
+        string path = "Assets/Demigiant/Editor/";
+        return (Texture)AssetDatabase.LoadAssetAtPath(path + name, typeof(Texture));
+    }
+
+    public void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 14;
+        style.normal.textColor = Color.white;
+        GUI.Label(this.textRect, "欢迎",style);
+        GUI.DrawTexture(this.imageRect, this.mSamplesImage);
+    }
+    
+    public static void ShowWindow()
+    {
+        WelcomeScreen window = EditorWindow.GetWindow<WelcomeScreen>(true, "Hello 你好 ");
+        window.minSize = window.maxSize = new Vector2(410f, 470f);
+        UnityEngine.Object.DontDestroyOnLoad(window);
+    }
+#endif
+}
+
+
